7081e9ba14010ef6226029bafc44018d610e7b6a
ltapsy <drolxiv@gmail.com>
2021-11-04 00:23:57 -0600

    fixing stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,9 @@ var menu = 0;
 function dropDownMenu(){
     if(menu == 0){
         menu++;
-        document.querySelector('.dropDown').style="top:60px !important; transition:0.5s;"
+        document.querySelector('.dropDown').style="margin-top:60px !important; transition:0.5s;"
     }else{
         menu--;
-        document.querySelector('.dropDown').style="top:-280px !important; transition:0.5s;"
+        document.querySelector('.dropDown').style="margin-top:-300px !important; transition:0.5s;"
     }
 }
